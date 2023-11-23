6ac01c68343fca4c480d4b08838832b037df2236
nitrozeus0g <106177077+nitrozeus0g@users.noreply.github.com>
2023-11-23 05:33:34 -0800

    .

---
+++ 2020/day01/day01.py
@@ -0,0 +1,12 @@
+from itertools import permutations as p
+
+def main(data):
+	pairs = p(data, 3)
+	for i in pairs:
+		if sum(i) == 2020:
+			return i[0]*i[1]*i[2]
+
+if __name__ == "__main__":
+	with open('inputs/day01.input') as f:
+		data = [int(line) for line in f.read().splitlines()]
+		print(main(data))
