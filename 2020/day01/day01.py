13b37798d6b968e3a0333f54fb2a2765417b23b9
nitrozeus0g <106177077+nitrozeus0g@users.noreply.github.com>
2023-11-23 06:59:59 -0800

    Correct

--- 2020/day01/day01.py
+++ 2020/day01/day01.py
@@ -4,9 +4,10 @@ def main(data):
 	pairs = p(data, 3)
 	for i in pairs:
 		if sum(i) == 2020:
-			return i[0]*i[1]*i[2]
+			return i[0] * i[1] * i[2]
 
 if __name__ == "__main__":
-	with open('inputs/day01.input') as f:
+	#with open('../sample.input') as f:
+	with open('../inputs/day01.input') as f:
 		data = [int(line) for line in f.read().splitlines()]
 		print(main(data))
